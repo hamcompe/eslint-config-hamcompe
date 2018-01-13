7daf7f430d6bd7befb0de61465219a28c482e109
Ham <pallop.chao@gmail.com>
2018-01-13 21:04:24 +0700

    :sparkles: Add `babel-eslint`

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   extends: 'airbnb',
+  parser: 'babel-eslint',
   rules: {
     semi: ['error', 'never'],
     'no-param-reassign': ['error', { props: false }],
@@ -10,6 +11,8 @@ module.exports = {
     'no-underscore-dangle': 'off',
     'react/jsx-filename-extension': 'off',
     'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off'
+    'react/prop-types': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off'
   }
 }
